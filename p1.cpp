048acf2035df712d62e22f9f421096ef706c4cb5
Mavani011 <163092454+Mavani011@users.noreply.github.com>
2024-03-12 10:56:12 +0530

    Create p1.cpp

---
+++ p1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+  int a,b,c;
+  char d[25];
+
+  cout<<"enter two number for addition";
+  cout<<"enter a:- ";
+  cin>>a;
+  cout<<"enter b:- ";
+  cin>>b;
+  c=a+b;
+  cout<<"additon of two number is :- "<<c<<endl;
+  cout<<"enter the number in spelling:- ";
+  cin>>d;
+  cout<<"spelling of number is :- "<<d<<endl;
+}
